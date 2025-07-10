e0e0e93c9f42ef1ee9d9e5b9e9815206bcf9155d
DungP <phamdunglili04012003@gmail.com>
2025-07-10 09:24:40 +0700

    add exercise 2.2: Course information step 7 part2

--- part2/courseinfo/src/components/course.jsx
+++ part2/courseinfo/src/components/course.jsx
@@ -1,9 +1,8 @@
-export const Part = ({part}) =>{
-    
-    return (
-        <p>{part.name} {part.exercises}</p>
-    )
+export const Total = ({parts}) =>{
+    const total = parts.reduce((sum, e) => sum + e.exercises, 0)
+    return <p><b>total of {total} exercises</b></p>
 }
+export const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
 export const Course = ({course}) =>{
 
@@ -11,6 +10,7 @@ export const Course = ({course}) =>{
         <>
             <h1>{course.name}</h1>
             {course.parts.map(part => <Part part = {part}/>)}
+            <Total parts={course.parts}/>
         </>
     )
 }
